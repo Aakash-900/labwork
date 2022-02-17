5bdbd663e54052ced2bc26b69769daeed34d7fae
Aakash-900 <aakashchdhry96@gmail.com>
2022-02-17 11:29:57 +0545

    update

---
+++ alo.py
@@ -0,0 +1,15 @@
+from re import I
+from telnetlib import DO
+
+
+# FUNCTION : sumoflist
+# INPUT : Read a list of type list
+# OUTPUT : total of type list
+# 1. total = 0
+# 2. FOR i in list DO
+# 3. total = total + i 
+# 4. RETURN total
+
+print("hello world")
+for i in range (10)
+count = count + 1
